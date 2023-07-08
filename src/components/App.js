b29e66efefb358cb020123765b277dacd64a8883
anissalopez <anissa965@gmail.com>
2023-07-08 16:09:13 -0400

    simple fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React, {useState, useEffect} from "react"
+
+
+function App(){
+
+        useEffect(()=>{
+
+            fetch("https://dog.ceo/api/breeds/image/random")
+            .then(res => res.json())
+            .then((data) => setImage(data.message))
+    }, [])
+
+    const [image, setImage] = useState(null)
+
+    if(!image){
+    return <p>Loading...</p>
+    }
+
+    return (
+       
+        <div>
+            <img src={image} alt="A Random Dog"></img>
+        </div>
+    )
+
+
+}
+
+export default App;
